fcca7ccec9cd148563a79bdce7c2288a1f5da0e4
Denis Konieiev <d.konieiev@gmail.com>
2018-10-25 21:08:51 +0300

    Created udpSockets

---
+++ udpServ.py
@@ -0,0 +1,23 @@
+import socket 
+
+def Main():
+	host = '127.0.0.1'
+	port = 5000
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind((host, port))
+
+	print "Server Started."
+	while True:
+		data, addr = s.recvfrom(1024)
+		print "message from: " + str(addr)
+		print "from connect user: " + str(data)
+		data = str(data).upper()
+		print "sending: " + str(data)
+		s.sendto(data, addr)
+	s.close()
+
+if __name__ == '__main__':
+	Main()
+
+
